32e7bad101da7e758e141ff6534bfe80b6286b1e
Kavita <kavita@bruviti.com>
2020-01-29 15:44:28 +0530

    gitignore file modified

--- app/build/generated/source/apt/debug/com/android/databinding/library/baseAdapters/BR.java
+++
@@ -1,8 +0,0 @@
-package com.android.databinding.library.baseAdapters;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int loginViewModel = 1;
-        public static final int password = 2;
-        public static final int userName = 3;
-}
--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="fb6ea926-19f3-4e3e-b963-f6826a722e75")
-public class DataBindingInfo {}
